39ae9dffab41b60f6c67bc7fa1254f455c1bdd99
Liam Dyer <liamcdyer@gmail.com>
2023-09-11 01:46:00 -0400

    feat: single cluster support, cillium, metrics server

--- control-plane/load-balancer.tf
+++ control-plane/load-balancer.tf
@@ -1,6 +1,11 @@
+locals {
+  control_plane_ipv4 = var.control_plane_count > 1 ? hcloud_load_balancer.control_plane.0.ipv4 : hcloud_primary_ip.control_plane.0.ip_address
+}
+
 # load balancer in front of the nodes
 resource "hcloud_load_balancer" "control_plane" {
-  name = "control-plane"
+  count = var.control_plane_count > 1 ? 1 : 0
+  name  = "control-plane"
   labels = {
     type = "control-plane"
   }
@@ -10,22 +15,24 @@ resource "hcloud_load_balancer" "control_plane" {
 
 # add nodes
 resource "hcloud_load_balancer_target" "load_balancer_target" {
-  for_each         = hcloud_server.control_plane
+  count            = var.control_plane_count > 1 ? var.control_plane_count : 0
   type             = "server"
-  load_balancer_id = hcloud_load_balancer.control_plane.id
-  server_id        = each.value.id
+  load_balancer_id = hcloud_load_balancer.control_plane.0.id
+  server_id        = hcloud_server.control_plane[count.index].id
 }
 
 # control plane service
 resource "hcloud_load_balancer_service" "load_balancer_service" {
-  load_balancer_id = hcloud_load_balancer.control_plane.id
+  count            = var.control_plane_count > 1 ? 1 : 0
+  load_balancer_id = hcloud_load_balancer.control_plane.0.id
   protocol         = "tcp"
   listen_port      = 6443
   destination_port = 6443
 }
 # talos CTL service
 resource "hcloud_load_balancer_service" "talosctl_service" {
-  load_balancer_id = hcloud_load_balancer.control_plane.id
+  count            = var.control_plane_count > 1 ? 1 : 0
+  load_balancer_id = hcloud_load_balancer.control_plane.0.id
   protocol         = "tcp"
   listen_port      = 50000
   destination_port = 50000
--- control-plane/servers.tf
+++ control-plane/servers.tf
@@ -4,6 +4,12 @@ variable "talos_version" {
   default     = "v1.5.2"
 }
 
+variable "control_plane_count" {
+  type        = number
+  description = "Number of control plane nodes"
+  default     = 1
+}
+
 data "hcloud_image" "talos" {
   with_selector = "os=talos,version=${var.talos_version}"
 }
@@ -16,16 +22,33 @@ resource "hcloud_placement_group" "control_plane" {
   }
 }
 
+resource "hcloud_primary_ip" "control_plane" {
+  count         = var.control_plane_count
+  name          = "control-plane-${count.index + 1}"
+  type          = "ipv4"
+  datacenter    = "fsn1-dc14"
+  assignee_type = "server"
+  auto_delete   = false
+  labels = {
+    "type" = "control-plane"
+  }
+}
+
 resource "hcloud_server" "control_plane" {
-  for_each = toset(["1", "2", "3"])
-  name     = "control-plane-${each.key}"
+  count = var.control_plane_count
+  name  = "control-plane-${count.index + 1}"
   labels = {
     type = "control-plane"
   }
   image              = data.hcloud_image.talos.id
   server_type        = "cpx11"
-  location           = "fsn1"
+  datacenter         = hcloud_primary_ip.control_plane[count.index].datacenter
   placement_group_id = hcloud_placement_group.control_plane.id
+  public_net {
+    ipv4_enabled = true
+    ipv4         = hcloud_primary_ip.control_plane[count.index].id
+    ipv6_enabled = true
+  }
 
   user_data = data.talos_machine_configuration.control_plane.machine_configuration
 }
